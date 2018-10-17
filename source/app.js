56e3c3855887865283ec545dab6acf8aec4a5850
Alessandro Maglioccola <alessandro@maglioccola.com>
2018-10-17 22:49:01 +0200

    Ch.4 Creazione componente container

--- source/app.js
+++ source/app.js
@@ -1,31 +1,5 @@
 var React = require('react');
-var createReactClass = require('create-react-class');
 var ReactDOM = require('react-dom');
+var Application = require('./components/Application.react');
 
-var reactClass = createReactClass({
-
-	getInitialState: function () {
-		return {
-			isHeaderHidden: true,
-			title: "stateful react component"
-		};
-	},
-
-	render: function () {
-
-		var headerElement = React.createElement("h1", { className: "header", key: "header" }, this.state.title);
-		var buttonElement = React.createElement("button", { className: "btn btn-default", onClick: this.handleClick, key: "button" }, "toggle header");
-
-		if (this.state.isHeaderHidden) {
-			return React.createElement("div", null, [buttonElement]);
-		}
-		return React.createElement("div", null, [buttonElement, headerElement]);
-	},
-
-	handleClick: function () {
-		this.setState({ isHeaderHidden: !this.state.isHeaderHidden });
-	}
-});
-
-var reactElement = React.createElement(reactClass);
-ReactDOM.render(reactElement, document.getElementById('react-application'));
+ReactDOM.render(<Application/>, document.getElementById('react-application'));
